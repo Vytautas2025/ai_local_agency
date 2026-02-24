95970aa26eb518710d8e0b0fc5f1f9ff287be73b
vytautas2025 <vytautas.briauka@gmail.com>
2026-02-24 12:24:04 +0000

    feat: add sitemap and robots configuration

---
+++ src/app/robots.ts
@@ -0,0 +1,35 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/api/',
+          '/_next/',
+          '/admin/',
+        ],
+      },
+      {
+        userAgent: 'GPTBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'ChatGPT-User',
+        allow: '/',
+      },
+      {
+        userAgent: 'PerplexityBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'ClaudeBot',
+        allow: '/',
+      },
+    ],
+    sitemap: 'https://tier3labs.co.uk/sitemap.xml',
+    host: 'https://tier3labs.co.uk',
+  }
+}
