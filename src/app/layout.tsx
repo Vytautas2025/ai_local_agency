ccfe0576ce73cbb62e3c0ce3edbec2626504441d
vytautas2025 <vytautas.briauka@gmail.com>
2026-04-28 14:04:57 +0100

    Add Meta Pixel base code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -138,6 +138,35 @@ export default async function RootLayout({
         <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
         <link rel="dns-prefetch" href="https://www.google-analytics.com" />
 
+        {/* Meta Pixel base code */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          nonce={nonce}
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '2806183149727513');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2806183149727513&ev=PageView&noscript=1"
+          />
+        </noscript>
+
         {/* Calendly CSS — loaded directly, it's ~4 KB and needed for the popup */}
         <link
           rel="stylesheet"
